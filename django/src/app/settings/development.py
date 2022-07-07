7f5d22585cc72696c7a6871e0b58f0dabbe00704
ryoutoku <invisible060@gmail.com>
2022-07-07 22:21:00 +0900

    feat: debugger

--- django/src/app/settings/development.py
+++ django/src/app/settings/development.py
@@ -1,10 +1,27 @@
 from .base import *  # noqa
-from .base import INSTALLED_APPS
+from .base import INSTALLED_APPS, MIDDLEWARE
 
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+MIDDLEWARE.extend([
+    'silk.middleware.SilkyMiddleware',
+])
+
 INSTALLED_APPS.extend([
     'drf_yasg',
+    'silk'
 ])
+
+SWAGGER_SETTINGS = {
+    'SECURITY_DEFINITIONS': {
+        'Bearer': {
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header',
+            'description': '`Bearer <token>`とする必要がある',
+        }
+    },
+    'USE_SESSION_AUTH': False,
+}
--- django/src/app/urls.py
+++ django/src/app/urls.py
@@ -15,7 +15,41 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+
+# DEBUG時のみ使用するdebugger
+if settings.DEBUG:
+    from django.urls import re_path, include
+    from rest_framework import permissions
+    from drf_yasg.views import get_schema_view
+    from drf_yasg import openapi
+
+    # swagger
+    schema_view = get_schema_view(
+        openapi.Info(
+            title="Swagger UI",
+            default_version='v1',
+            description="UI for debug",
+        ),
+        public=True,
+        permission_classes=[permissions.AllowAny],
+    )
+
+    urlpatterns.extend([
+        re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+                schema_view.without_ui(cache_timeout=0), name='schema-json'),
+        re_path(r'^swagger/$', schema_view.with_ui('swagger',
+                cache_timeout=0), name='schema-swagger-ui'),
+        re_path(r'^redoc/$', schema_view.with_ui('redoc',
+                cache_timeout=0), name='schema-redoc'),
+    ])
+
+    # silk
+    urlpatterns.extend([
+        path('silk/', include('silk.urls', namespace='silk'))]
+    )
