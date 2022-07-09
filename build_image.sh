635f2148800c33ff5378695072b2b1a39eec04ca
ryoutoku <invisible060@gmail.com>
2022-07-09 23:17:04 +0900

    feat: image作成バッチ

---
+++ build_image.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+image=${1:-django}
+tag=${2:-latest}
+
+# create production docker image
+echo -e '\e[34m##############################\e[m'
+echo -e build ${image}:${tag}
+echo -e '\e[34m##############################\e[m'
+docker build ./django --target=production -t ${image}:${tag}
+
+if type dockle > /dev/null 2>&1; then
+    # check security using dockle 
+    dockle -af settings.py ${image}:${tag}
+fi
